94e1252ed70b61064b439fad3f3aee88e5bc277e
vrushaliw <vrushali@amuratech.com>
2018-08-12 13:51:12 +0530

    Changes in style according to devices

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-if(document.documentElement.clientWidth < 500){
-  // if(document.getElementsByClassName('image-title-section')[0].clientHeight < document.documentElement.clientHeight){
-  //   document.getElementsByClassName('image-title-section')[0].style.height = document.documentElement.clientHeight + "px"
-  // }
-}else{
-  if(document.getElementById('introduction-section').clientHeight < document.documentElement.clientHeight){
-    document.getElementById('introduction-section').style.height = document.documentElement.clientHeight + "px"
-  }
-}
+// if(document.documentElement.clientWidth > 1104){
+//   // if(document.getElementsByClassName('image-title-section')[0].clientHeight < document.documentElement.clientHeight){
+//   //   document.getElementsByClassName('image-title-section')[0].style.height = document.documentElement.clientHeight + "px"
+//   // }
+// }else{
+//   if(document.getElementById('introduction-section').clientHeight < document.documentElement.clientHeight){
+//     document.getElementById('introduction-section').style.height = document.documentElement.clientHeight + "px"
+//   }
+// }
